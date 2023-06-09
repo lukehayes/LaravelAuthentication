aeb31dde9e9fc2530a5a97fa3bf35673027be21f
lukehayes <lukepurplehayes@gmail.com>
2023-06-09 17:49:41 +0100

    Added form credentials to LoginController::authenticate().

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
     public function authenticate(Request $request)
     {
-        dd($request);
+        // TODO Implement manual authentication.
+        $credentials = $request->validate([
+            'username' => ['required', 'email'],
+            'password' => ['required']
+        ]);
+
+        dd(Auth::attempt($credentials));
     }
 }
