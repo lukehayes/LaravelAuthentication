4ae5c7e9708024c16e1d49137788f5504a12bf34
lukehayes <lukepurplehayes@gmail.com>
2023-06-15 14:18:14 +0100

    Added tailwindcss to project.

--- resources/views/page/app.blade.php
+++ resources/views/page/app.blade.php
@@ -1,6 +1,9 @@
 <html>
     <head>
         <title>App Name - @yield('title')</title>
+
+        @vite('resources/css/app.css')
+        <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css"> -->
     </head>
     <body>
         <div class="container">
