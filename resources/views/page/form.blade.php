5715b2a05cbeec415f5831b159d93febc7d1cb64
Luke Hayes <lukepurplehayes@gmail.com>
2023-06-19 11:53:24 +0100

    Added simple tailwind styles to login form.

--- resources/views/page/form.blade.php
+++ resources/views/page/form.blade.php
@@ -4,6 +4,8 @@
 
 @section('content')
 
+<div class="mx-auto h-screen">
+
     @if($errors->any())
         <div id="form-errors">
         @foreach($errors->all() as $error)
@@ -12,10 +14,11 @@
         </div>
     @endif
 
-    <form method="POST" action="{{route('authenticate')}}">
+    <form method="POST" action="{{route('authenticate')}}" class='bg-white p-5 m-5 rounded'>
         {{csrf_field()}}
-        <input type="text" name="username" placeholder='username'>
-        <input type="password" name="password" placeholder='password'>
-        <input type="submit" value='login'>
+        <input type="text" name="username" placeholder='username' class='border block p-5 m-auto w-full my-5'>
+        <input type="password" name="password" placeholder='password' class='border block p-5 mx-auto w-full my-5'>
+        <input type="submit" value='login' class='my-4 p-5 bg-gray-700 rounded w-full'>
     </form>
+</div>
 @endsection
